2c5c46e3d7cdd6e680956969ecb2aa42e67274ce
podlesniy <podlesniy.1992@gmail.com>
2022-09-08 20:52:29 +0300

    Complete SevenApp

--- SevenApp/app/src/main/java/com/example/sevenapp/MyService.kt
+++ SevenApp/app/src/main/java/com/example/sevenapp/MyService.kt
@@ -1,6 +1,5 @@
 package com.example.sevenapp
 
-import android.util.Log
 import android.app.Service
 import android.content.Intent
 import android.hardware.Sensor
@@ -8,9 +7,17 @@ import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.IBinder
+import android.util.Log
 import android.widget.Toast
+import kotlinx.coroutines.*
 
 class MyService : Service(), SensorEventListener {
+
+    companion object {
+        var ax: String? = null
+        var ay: String? = null
+        var az: String? = null
+    }
 
     private lateinit var sensorManager: SensorManager
 
@@ -22,19 +29,25 @@ class MyService : Service(), SensorEventListener {
         super.onCreate()
         sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
         sensorManager.registerListener(this, sensorManager.getDefaultSensor(
-            Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL)
+            Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_UI)
 
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        Toast.makeText(this, "Hello from service", Toast.LENGTH_SHORT).show()
+//        var delay = 5000L
+//        GlobalScope.launch {
+//            while (delay < 200000)
+//            delay(2000L)
+
+//            delay+=2000
+//        }
         return START_NOT_STICKY
     }
 
     override fun onDestroy() {
         super.onDestroy()
         sensorManager.unregisterListener(this)
-        Toast.makeText(this, "Service stopped", Toast.LENGTH_SHORT).show()
+//        Toast.makeText(this, "Service stopped", Toast.LENGTH_SHORT).show()
 
     }
     
@@ -43,10 +56,11 @@ class MyService : Service(), SensorEventListener {
     }
 
     override fun onSensorChanged(event: SensorEvent) {
-        val ax = event.values[0]
-        val ay = event.values[1]
-        val az = event.values[2]
 
+        ax = event.values[0].toString()
+        ay = event.values[1].toString()
+        az = event.values[2].toString()
+//        Toast.makeText(this@MyService, ax.toString()+ay.toString()+az.toString(), Toast.LENGTH_SHORT).show()
         Log.d("sensor","$ax $ay $az")
     }
 }
